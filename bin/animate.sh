b85b7c9324386e6f0290fb2d68d15cf6cc39cb20
Jeff Rodriguez <jeff@jeffrodriguez.com>
2016-09-24 00:49:54 -0700

    Animate should 'nice' the commands.

--- bin/animate.sh
+++ bin/animate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-convert -delay 5 -loop 0 frame*.png animation.gif
+nice convert -delay 5 -loop 0 frame*.png animation.gif
 
-ffmpeg -i frame%05d.png -framerate 10 -s:v 1920x1080 -c:v libx264 -crf 20 -pix_fmt yuv420p animation.mp4
+nice ffmpeg -i frame%05d.png -framerate 10 -s:v 1920x1080 -c:v libx264 -crf 20 -pix_fmt yuv420p animation.mp4
