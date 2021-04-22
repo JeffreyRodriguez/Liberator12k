867b3f67a06cda45d2bae4b9f4a86f62664635ec
Jeff Rodriguez <jeff@jeffrodriguez.com>
2021-04-21 18:20:50 -0700

    Don't autopreview, messes with make.

--- bin/animate.sh
+++ bin/animate.sh
@@ -6,6 +6,4 @@ if [ -z "$1" ]
   exit 1
 fi
 
-nice ffmpeg -loop 1 -t 60 -y -i "$1/frame%05d.png" -framerate 10 -s:v 1920x1080 -c:v libx264 -crf 20 -pix_fmt yuv420p "$2" && \
-vlc "$2"
-
+nice ffmpeg -loop 1 -t 60 -y -i "$1/frame%05d.png" -framerate 10 -s:v 1920x1080 -c:v libx264 -crf 20 -pix_fmt yuv420p "$2"
